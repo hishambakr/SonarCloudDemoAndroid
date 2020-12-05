e1fb4b538976791d0ef2c6e199946d6c7665a2cd
hisham Bakr <hisham.hassan.bakr@gmail.com>
2020-12-05 20:14:38 +0400

    ss

---
+++ app/src/main/java/com/hisham/sonarclouddemo/MyKotlinClas2.kt
@@ -0,0 +1,19 @@
+package com.hisham.sonarclouddemo
+
+class MyKotlinClas2 {
+    fun myPlus(a: Int, b: Int): Int{
+        return a+b
+    }
+
+    fun myMinus(a: Int, b: Int): Int{
+        return a-b
+    }
+
+    fun myMinus2(a: Int, b: Int): Int{
+        return a-b
+    }
+
+    fun myMinus3(a: Int, b: Int): Int{
+        return a-b
+    }
+}
